cdbb2c48009eafeebaaf895b467f98f9062a2c3a
alexhiguera <alexhigueramartinez@gmail.com>
2024-02-26 22:35:20 +0100

    update: include stdbool.h

--- src/libft.h
+++ src/libft.h
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
+# include <stdbool.h>
 
 /* ------------------------------------------------------------------------- */
 //libft prototypes
