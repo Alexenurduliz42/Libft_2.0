99362cb78d1dd46b25774252fb937a490a53e322
alexhiguera <alexhigueramartinez@gmail.com>
2024-02-26 22:37:13 +0100

    refactor

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -18,22 +18,21 @@ int	ft_atoi(const char *str)
 	int	sign;
 	int	result;
 
-	sign = 1;
-	result = 0;
 	i = 0;
 	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
 		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
 		i++;
+	sign = 1;
 	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
-			sign = -1;
+			sign *= -1;
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		result = result * 10 + (str[i] - '0');
+		result = (result * 10) + (str[i] - '0');
 		i++;
 	}
-	return (result * sign);
+	return (sign * result);
 }
